8816e32058a0ec6d9c35870117f4128082a7e2a6
Charlie Ruffell (School) <1cruffel@hbhs.school.nz>
2024-05-30 13:59:51 +1200

    Add project files.

---
+++ Fall-DownV2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Fall_DownV2
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.screen = new System.Windows.Forms.Panel();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.gameTick = new System.Windows.Forms.Timer(this.components);
+            this.floor = new System.Windows.Forms.PictureBox();
+            this.objectsUpdater = new System.Windows.Forms.Timer(this.components);
+            this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.Controls.Add(this.floor);
+            this.screen.Controls.Add(this.player);
+            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(800, 450);
+            this.screen.TabIndex = 0;
+            this.screen.Tag = "platform";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.player.Location = new System.Drawing.Point(493, 289);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(25, 25);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            this.player.Tag = "player";
+            // 
+            // gameTick
+            // 
+            this.gameTick.Enabled = true;
+            this.gameTick.Interval = 1;
+            this.gameTick.Tick += new System.EventHandler(this.gameTick_Tick);
+            // 
+            // floor
+            // 
+            this.floor.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.floor.Location = new System.Drawing.Point(0, 426);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(800, 24);
+            this.floor.TabIndex = 1;
+            this.floor.TabStop = false;
+            // 
+            // objectsUpdater
+            // 
+            this.objectsUpdater.Enabled = true;
+            this.objectsUpdater.Interval = 10;
+            this.objectsUpdater.Tick += new System.EventHandler(this.objectsUpdater_Tick);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.screen);
+            this.Name = "Game";
+            this.Text = "Get Down";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Game_KeyUp);
+            this.screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer gameTick;
+        private System.Windows.Forms.PictureBox floor;
+        private System.Windows.Forms.Timer objectsUpdater;
+    }
+}
+
---
+++ Fall-DownV2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fall_DownV2
+{
+    public partial class Game : Form
+    {
+        //declare bools for movement, gravity etc.
+        bool left = false, right = false, falling = false;
+
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private void objectsUpdater_Tick(object sender, EventArgs e)
+        {
+            floor.Width = screen.Width;
+            floor.Left = 0;
+            floor.Top = screen.Height - floor.Height;
+        }
+
+        private void Game_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                left = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                right = false;
+            }
+        }
+
+        private void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                left = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                right = true;
+            }
+        }
+        private void gameTick_Tick(object sender, EventArgs e)
+        {
+            if (left == true)
+            {
+                player.Left -= 5;
+            }
+            if (right == true)
+            {
+                player.Left += 5;
+            }
+        }
+    }
+}
